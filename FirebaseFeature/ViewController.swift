2e39fa1f9f9e99ca79058d812b8ec0ba979ba0bf
zharif <zharif@ig-interactive.com>
2018-06-09 16:13:57 +0800

    first commit

--- FirebaseFeature/ViewController.swift
+++ FirebaseFeature/ViewController.swift
@@ -7,17 +7,22 @@
 //
 
 import UIKit
+import Crashlytics
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+        Crashlytics.sharedInstance().crash()
     }
 
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FirebaseFeature' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FirebaseFeature
+pod 'Firebase/Core'
+pod 'Fabric', '~> 1.7.7'
+pod 'Crashlytics', '~> 3.10.2'
+end

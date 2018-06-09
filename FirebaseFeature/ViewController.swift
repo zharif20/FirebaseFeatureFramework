0d17508f6928822f8e2ebd842add1165e63a1bf6
zharif <zharif@ig-interactive.com>
2018-06-09 15:02:37 +0800

    Initial Commit

---
+++ FirebaseFeature/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseFeature
+//
+//  Created by M. Zharif Hadi M. Khairuddin on 09/06/2018.
+//  Copyright © 2018 M. Zharif Hadi M. Khairuddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
